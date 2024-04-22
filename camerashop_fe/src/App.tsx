8c35d219a221e28aff2ef853ec61b18e7dccb9fc
hiepnx03 <hiepnx03@gmail.com>
2024-04-22 18:50:03 +0700

    upload

---
+++ camerashop_fe/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Footer from './layouts/header-footer/Footer';
+import Navbar from './layouts/header-footer/Navbar';
+import ProductList from './layouts/products/ProductList';
+import ProductManagement from "./admin/ProductManagement";
+
+function App() {
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<ProductList />} />
+                    <Route path="/admin" element={<ProductManagement />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </div>
+    );
+}
+
+export default App;
