abffacc8a57eff4db1f6a8378873a4c8829c56b9
hiepnx03 <hiepnx03@gmail.com>
2024-09-25 12:24:00 +0700

    update

--- camerashop_be1/src/main/java/com/example/laptop_be/repository/FavoriteProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.laptop_be.repository;
-
-import com.example.laptop_be.entity.FavoriteProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FavoriteProductRepository extends JpaRepository<FavoriteProduct, Integer> {
-}
